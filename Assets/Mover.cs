a8cbc73d483dbf0d0d586a58b7017f2c3ae0d228
Michal Orzechowski <Michal.Orzechowski@cdsazure.onmicrosoft.com>
2019-07-31 15:33:43 +0200

    #1

---
+++ Assets/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("walking"))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + (transform.forward * 0.1f), 1.0f * Time.deltaTime);
+        }
+    }
+}
