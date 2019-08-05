ec85866701c82ae061ae1c879fe038f6bc395afb
Michal Orzechowski <Michal.Orzechowski@cdsazure.onmicrosoft.com>
2019-08-05 13:28:01 +0200

    PlayerController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera camera;
+    public NavMeshAgent agent;
+
+    private InputManager _inputManager;
+    private List<IPoi> _pois;
+
+    [Inject]
+    void Initialize(InputManager inputManager, List<IPoi> pois)
+    {
+        _inputManager = inputManager;
+        _pois = pois;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_inputManager.WasClicked)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+        
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (Mathf.Abs(agent.velocity.x) > 0 || Mathf.Abs(agent.velocity.y) > 0 || Mathf.Abs(agent.velocity.z) > 0)
+        {
+            GetComponent<Animator>().SetBool("isWalking", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("isWalking", false);
+        }
+    }
+}
